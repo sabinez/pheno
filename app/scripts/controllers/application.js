1408a70fc9fabe60e886587dc84b79c72a111ca1
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2015-12-06 20:37:12 +0100

    refactor setTimeBarStyle function

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -25,45 +25,30 @@
       };
 
       var setTimeBarStyle = function(){
-        var sowingWidth, sowingMargin, sowingWidthSecond, sowingMarginSecond, plantingWidth, plantingMargin, harvestingWidth, harvestingMargin, harvestingWidthSecond, harvestingMarginSecond;
         var monthWidth = getMonthWidth();
         for(var i = 0; i < $scope.PLANTS.length; i++){
           var plant = $scope.PLANTS[i];
-          sowingWidth = calculateTimeBarWidth(plant.TIMES.SOWING, 1, monthWidth);
-          sowingMargin = calculateTimeBarMargin(plant.TIMES.SOWING, 1, monthWidth);
-          if ( plant.TIMES.SOWING.length > 2 ) {
-            sowingWidthSecond = calculateTimeBarWidth(plant.TIMES.SOWING, 2, monthWidth);
-            sowingMarginSecond = calculateTimeBarMargin(plant.TIMES.SOWING, 2, monthWidth);
-          } else {
-            sowingWidthSecond = 0;
-            sowingMarginSecond = 0;
+
+          var cultivationSteps = [ "sowing", "planting", "harvesting" ];
+          for(var j = 0; j < cultivationSteps.length; j++){
+            var cultivationStepWidth = cultivationSteps[j] + "Width";
+            var cultivationStepMargin = cultivationSteps[j] + "Margin";
+            var cultivationStepWidthSecond = cultivationSteps[j] + "WidthSecond";
+            var cultivationStepMarginSecond = cultivationSteps[j] + "MarginSecond";
+            var cultivationStepUpperCase = cultivationSteps[j].toUpperCase();
+            var plantTime = plant.TIMES[cultivationStepUpperCase];
+
+            if ( plantTime === undefined ){ continue; }
+            $scope.PLANTS[i][cultivationStepWidth] = calculateTimeBarWidth(plantTime, 1, monthWidth);
+            $scope.PLANTS[i][cultivationStepMargin] = calculateTimeBarMargin(plantTime, 1, monthWidth);
+            if ( plantTime.length > 2 ) {
+              $scope.PLANTS[i][cultivationStepWidthSecond] = calculateTimeBarWidth(plantTime, 2, monthWidth);
+              $scope.PLANTS[i][cultivationStepMarginSecond] = calculateTimeBarMargin(plantTime, 2, monthWidth);
+            } else {
+              $scope.PLANTS[i][cultivationStepWidthSecond] = 0;
+              $scope.PLANTS[i][cultivationStepMarginSecond] = 0;
+            }
           }
-          if ( plant.TIMES.PLANTING ) {
-            plantingWidth = calculateTimeBarWidth(plant.TIMES.PLANTING, 1, monthWidth);
-            plantingMargin = calculateTimeBarMargin(plant.TIMES.PLANTING, 1, monthWidth);
-          } else {
-            plantingWidth = 0;
-            plantingMargin = 0;
-          }
-          harvestingWidth = calculateTimeBarWidth(plant.TIMES.HARVESTING, 1, monthWidth);
-          harvestingMargin = calculateTimeBarMargin(plant.TIMES.HARVESTING, 1, monthWidth);
-          if ( plant.TIMES.SOWING.length > 2 ) {
-            harvestingWidthSecond = calculateTimeBarWidth(plant.TIMES.HARVESTING, 2, monthWidth);
-            harvestingMarginSecond = calculateTimeBarMargin(plant.TIMES.HARVESTING, 2, monthWidth);
-          } else {
-            harvestingWidthSecond = 0;
-            harvestingMarginSecond = 0;
-          }
-          $scope.PLANTS[i].sowingWidth = sowingWidth;
-          $scope.PLANTS[i].sowingMargin = sowingMargin;
-          $scope.PLANTS[i].sowingWidthSecond = sowingWidthSecond;
-          $scope.PLANTS[i].sowingMarginSecond = sowingMarginSecond;
-          $scope.PLANTS[i].plantingWidth = plantingWidth;
-          $scope.PLANTS[i].plantingMargin = plantingMargin;
-          $scope.PLANTS[i].harvestingWidth = harvestingWidth;
-          $scope.PLANTS[i].harvestingMargin = harvestingMargin;
-          $scope.PLANTS[i].harvestingWidthSecond = harvestingWidthSecond;
-          $scope.PLANTS[i].harvestingMarginSecond = harvestingMarginSecond;
         }
       };
 
