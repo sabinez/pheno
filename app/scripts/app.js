2075d50eba9dd86fa33b481530370295b43ae9e2
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-01-27 19:31:25 +0100

    remove views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,24 +8,3 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/overview.html',
-        controller: 'OverviewCtrl',
-        controllerAs: 'overview'
-      })
-      .when('/:plantName', {
-        templateUrl: 'views/detail.html',
-        controller: 'DetailCtrl',
-        controllerAs: 'detail'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
--- app/scripts/controllers/overview.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('phenoApp')
-  .controller('OverviewCtrl', function () {
-
-  });
