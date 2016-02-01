66ec378b32aa63eef6aeb142b2b4e4a2a0709875
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-02-01 18:38:20 +0100

    pin calendar, animate side bar to slide to the left, pin sidebar at the left while scrolling through the vegetable screens

--- app/scripts/scrolling.js
+++ app/scripts/scrolling.js
@@ -3,10 +3,24 @@
 $(function () {
   var scrollingController = new ScrollMagic.Controller();
 
-  var sideBar = new ScrollMagic.Scene({triggerElement: "#trigger-side-bar", duration: $(document).height()})
+  var sideBarTween = new TweenMax.to("#target-side-bar-tween", 1, {left: '5px'});
+
+  var calendar = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 1.5 * $(window).height()})
     .triggerHook(0)
-    .setPin("#target-side-bar", {pushFollowers: false})
+    .setPin("#target-calendar", {pushFollowers: true})
     .addTo(scrollingController);
+
+  var sideBarTween = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
+    .triggerHook(0)
+    .offset($(window).height())
+    .setTween(sideBarTween)
+    .addTo(scrollingController);
+
+  var sideBar = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-frame", duration: $(document).height()})
+    .triggerHook(1)
+    .setPin("#target-side-bar", {pushFollowers: false})
+    .addTo(scrollingController)
+    .addIndicators();
 
   var tomatoFrame = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-frame", duration: 2.2 * $(window).height()
 })
