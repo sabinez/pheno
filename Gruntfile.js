178fdd14ab999c197a4a686d9545fde6c0e18c53
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-02-09 23:06:12 +0100

    add *.json to grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,7 +393,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,json}',
             '.htaccess',
             '*.html',
             'images/{,*/}*.{webp}',
