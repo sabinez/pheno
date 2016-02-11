2672264cebd7f9aa2106cb3fa26d0b786e79f59d
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-02-12 00:33:30 +0100

    add pepper and beetroot, animate scrolling of pepper and beetroot

--- app/scripts/services/scrolling.js
+++ app/scripts/services/scrolling.js
@@ -10,6 +10,7 @@
       var sideBarTween = new TweenMax.to("#target-side-bar-tween", 1, {left: '5px'});
       var sideBarLinesTween = new TweenMax.to("#target-side-bar-lines-tween", 1, {right: '97vw'});
       var sideBarLinksTween = new TweenMax.to(".target-side-bar-links-tween", 1, {opacity: '0.3'});
+      var beetrootTween = new TweenMax.to(['#target-beetroot-img', '#target-pepper-img'], 1, {'margin-left': '-50vw'});
 
       var calendar = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 *  $(window).height()})
         .triggerHook(0)
@@ -63,7 +64,7 @@
         .setPin("#target-tomato-text", {pushFollowers: false})
         .addTo(scrollingController);
 
-      var sweetPotatoImg = new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-img", duration: $(window).height()})
+      var sweetPotatoImg = new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-img", duration: 0.65 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-sweet-potato-img", {pushFollowers: false})
         .setClassToggle("#side-bar-sweet-potato", "active")
@@ -72,6 +73,30 @@
       var sweetPotatoText = new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-text", duration: 0.4 * $(window).height()})
         .triggerHook(0.55)
         .setPin("#target-sweet-potato-text", {pushFollowers: false})
+        .addTo(scrollingController);
+
+      var pepperImg = new ScrollMagic.Scene({triggerElement: "#trigger-pepper-img", duration: 4 * $(window).height()})
+        .triggerHook(0)
+        .setPin("#target-pepper-img", {pushFollowers: false})
+        .setClassToggle("#side-bar-pepper", "active")
+        .addTo(scrollingController);
+
+      var pepperText = new ScrollMagic.Scene({triggerElement: "#trigger-pepper-text", duration: 0.4 * $(window).height()})
+        .triggerHook(0.4)
+        .setPin("#target-pepper-text", {pushFollowers: false})
+        .addTo(scrollingController);
+
+      var beetrootImg = new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-img", duration: 1.7 * $(window).height()})
+        .triggerHook(0)
+        .setPin("#target-beetroot-img", {pushFollowers: false})
+        .setClassToggle("#side-bar-beetroot", "active")
+        .addTo(scrollingController);
+
+      var beetrootText = new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-text", duration: 0.6 * $(window).height()})
+        .triggerHook(0.3)
+        .setPin("#target-beetroot-text", {pushFollowers: false})
+        .setTween(beetrootTween)
+        .setClassToggle("#trigger-beetroot-text", "visible")
         .addTo(scrollingController);
 
       scrollingController.scrollTo(function (newpos) {
