f3575401d5347f6508849e06a6e2041d1d5af216
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-04-04 13:22:23 +0200

    fix that scenes will recalculate on window resize

--- app/scripts/services/scrolling.js
+++ app/scripts/services/scrolling.js
@@ -2,6 +2,7 @@
   'use strict';
 
   angular.module('phenoApp').service('initScrolling', function () {
+    var sceneCollection = [];
 
     var initScrolling = function() {
 
@@ -15,140 +16,163 @@
       var brusselSproutsRotation = new TweenMax.to("#target-brussel-sprouts-cutout", 1, {rotation: 360, ease: Linear.easeNone});
 
 
-      var calendar = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 *  $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 *  $(window).height()})
         .triggerHook(0)
         .setPin("#target-calendar", {pushFollowers: true})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sideBarTween = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
         .triggerHook(0)
         .offset(0.5 * $(window).height())
         .setTween(sideBarTween)
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sideBarLinksTween = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
         .triggerHook(0)
         .offset(0.5 * $(window).height())
         .setTween(sideBarLinksTween)
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sideBarLinesTween = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 0.5 * $(window).height()})
         .triggerHook(0)
         .offset(0.5 * $(window).height())
         .setTween(sideBarLinesTween)
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sideBar = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-hg", duration: $('#viewport')[0].scrollHeight})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-tomato-hg", duration: $('#viewport')[0].scrollHeight})
         .triggerHook(1)
         .setPin("#target-side-bar", {pushFollowers: false})
         .setClassToggle("#target-side-bar", "side-bar")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sideBarLines = new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: $('#viewport')[0].scrollHeight})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: $('#viewport')[0].scrollHeight})
         .triggerHook(0)
         .offset(0.5 *  $(window).height())
         .setPin("#target-side-bar-lines", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var tomatoHg = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-hg", duration: 1.3 * $(window).height()
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-tomato-hg", duration: 1.3 * $(window).height()
     })
         .triggerHook(0)
         .setPin("#target-tomato-hg", {pushFollowers: false})
         .setClassToggle("#side-bar-tomato", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var tomatoImg = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-img", duration: 1.15 * $(window).height() })
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-tomato-img", duration: 1.15 * $(window).height() })
         .triggerHook(0)
         .setPin("#target-tomato-img", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var tomatoText = new ScrollMagic.Scene({triggerElement: "#trigger-tomato-text", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-tomato-text", duration: 0.5 * $(window).height()})
         .triggerHook(0.4)
         .setPin("#target-tomato-text", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sweetPotatoImg = new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-img", duration: 0.65 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-img", duration: 0.65 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-sweet-potato-img", {pushFollowers: false})
         .setClassToggle("#side-bar-sweet-potato", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var sweetPotatoText = new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-text", duration: 0.4 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-sweet-potato-text", duration: 0.4 * $(window).height()})
         .triggerHook(0.55)
         .setPin("#target-sweet-potato-text", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var pepperImg = new ScrollMagic.Scene({triggerElement: "#trigger-pepper-img", duration: 3 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-pepper-img", duration: 3 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-pepper-img", {pushFollowers: false})
         .setClassToggle("#side-bar-pepper", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var pepperText = new ScrollMagic.Scene({triggerElement: "#trigger-pepper-text", duration: 0.4 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-pepper-text", duration: 0.4 * $(window).height()})
         .triggerHook(0.4)
         .setPin("#target-pepper-text", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var beetrootImg = new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-img", duration: 1.2 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-img", duration: 1.2 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-beetroot-img", {pushFollowers: false})
         .setClassToggle("#side-bar-beetroot", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var beetrootText = new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-text", duration: 0.3 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-beetroot-text", duration: 0.3 * $(window).height()})
         .triggerHook(0.3)
         .setPin("#target-beetroot-text", {pushFollowers: false})
         .setTween(beetrootTween)
         .setClassToggle("#trigger-beetroot-text", "visible")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var peasImg = new ScrollMagic.Scene({triggerElement: "#trigger-peas-img", duration: 1.25 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-peas-img", duration: 1.25 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-peas-img", {pushFollowers: false})
         .setClassToggle("#side-bar-peas", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var carrotImg = new ScrollMagic.Scene({triggerElement: "#trigger-carrot-img", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-carrot-img", duration: 0.5 * $(window).height()})
         .triggerHook(0.3)
         .setTween(carrotRotation)
         .setPin("#target-carrot-img", {pushFollowers: false})
         .setClassToggle("#side-bar-carrot", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var chardImg = new ScrollMagic.Scene({triggerElement: "#trigger-chard-img", duration: 1.3 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-chard-img", duration: 1.3 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-chard-img", {pushFollowers: false})
         .setClassToggle("#side-bar-chard", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var brusselSprouts = new ScrollMagic.Scene({triggerElement: "#trigger-brussel-sprouts", duration: 0.5 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-brussel-sprouts", duration: 0.5 * $(window).height()})
         .triggerHook(0)
         .setTween(brusselSproutsRotation)
         .setPin("#trigger-brussel-sprouts", {pushFollowers: false})
         .setClassToggle("#side-bar-brussel-sprouts", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var spinachImg = new ScrollMagic.Scene({triggerElement: "#trigger-spinach-img", duration: 0.9 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-spinach-img", duration: 0.9 * $(window).height()})
         .triggerHook(0)
         .setPin("#target-spinach-img", {pushFollowers: false})
         .setClassToggle("#side-bar-spinach", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var spinachText = new ScrollMagic.Scene({triggerElement: "#trigger-spinach-text", duration: 0.3 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-spinach-text", duration: 0.3 * $(window).height()})
         .triggerHook(0.6)
         .setPin("#target-spinach-text", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var onionText = new ScrollMagic.Scene({triggerElement: "#trigger-onion-text", duration: 1.2 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-onion-text", duration: 1.2 * $(window).height()})
         .triggerHook(0.7)
         .setPin("#target-onion-text", {pushFollowers: false})
         .setClassToggle("#side-bar-onion", "active")
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
-      var onionImg = new ScrollMagic.Scene({triggerElement: "#trigger-onion-img", duration: 0.2 * $(window).height()})
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-onion-img", duration: 0.2 * $(window).height()})
         .triggerHook(0.1)
         .setPin("#target-onion-img", {pushFollowers: false})
-        .addTo(scrollingController);
+        .addTo(scrollingController)
+      );
 
       scrollingController.scrollTo(function (newpos) {
         TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
@@ -166,6 +190,18 @@
       });
 
     };
+
+    $(window).on('resize', function() {
+
+      for (var i = 0; i < sceneCollection.length; i++) {
+        sceneCollection[i].destroy(true);
+      };
+
+      sceneCollection = [];
+
+      initScrolling();
+
+    });
 
     return initScrolling;
 
