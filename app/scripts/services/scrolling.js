e9b0c89778e67e883e225ec3ced99e5a3bf93342
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-04-19 12:16:48 +0200

    add tween to slide time bars to the left

--- app/scripts/services/scrolling.js
+++ app/scripts/services/scrolling.js
@@ -8,9 +8,10 @@
 
       var scrollingController = new ScrollMagic.Controller({container: "#viewport"});
 
-      var sideBarTween = new TweenMax.to("#target-side-bar-tween", 1, {left: '-20vh'});
+      var sideBarTween = new TweenMax.to("#target-side-bar-tween", 1, {left: '-100vh'});
       var sideBarLinesTween = new TweenMax.to(".plant-separator-line", 1, {width: '0'});
       var calendarMonthLinesTween = new TweenMax.to(".month-separators", 1, {height: '0'});
+      var calendarContentTween = new TweenMax.to(".calendar-content", 1, {left: '-85vh'});
       var beetrootTween = new TweenMax.to(['#target-beetroot-img', '#target-pepper-img'], 1, {'margin-left': '-50vw'});
       var carrotRotation = new TweenMax.to("#target-carrot-rotation", 1, {rotation: 180, ease: Linear.easeNone});
       var brusselSproutsRotation = new TweenMax.to("#target-brussel-sprouts-cutout", 1, {rotation: 360, ease: Linear.easeNone});
@@ -19,7 +20,6 @@
       sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 1.5 *  $(window).height()})
         .triggerHook(0)
         .setPin("#target-calendar", {pushFollowers: true})
-        .addIndicators()
         .addTo(scrollingController)
       );
 
@@ -27,6 +27,13 @@
         .triggerHook(0)
         .offset(0.5 * $(window).height())
         .setTween(sideBarTween)
+        .addTo(scrollingController)
+      );
+
+      sceneCollection.push( new ScrollMagic.Scene({triggerElement: "#trigger-calendar", duration: 1 * $(window).height()})
+        .triggerHook(0)
+        .offset(0.5 * $(window).height())
+        .setTween(calendarContentTween)
         .addTo(scrollingController)
       );
 
