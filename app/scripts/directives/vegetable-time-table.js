7f0c19a0a86bcb92f525f3d307775cbeffa9ab87
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2016-06-07 23:01:02 +0200

    fix: use correct month names

--- app/scripts/directives/vegetable-time-table.js
+++ app/scripts/directives/vegetable-time-table.js
@@ -21,12 +21,16 @@ angular.module('phenoApp')
           "Nov",
           "Dec"
         ]
-        scope.monthNameFromNumber = function(number){
+        scope.monthNameFromNumber = function(number, isEnd){
+          number = number - 1;
           if(!number){ return; };
-          if( number % 1 === 0 ){ //check if number is an integer
+          if( number % 1 !== 0 ){ //check if number is an float
+            var monthName = "Mid " + months[Math.floor(number)];
+          } else if(isEnd) {
+            number = number -1;
             var monthName = months[number];
           } else {
-            var monthName = "Mid " + months[Math.floor(number)];
+            var monthName =  months[number];
           };
           return monthName;
       };
@@ -35,23 +39,23 @@ angular.module('phenoApp')
         <table>\
           <tr>\
             <td>sow</td>\
-            <td>{{monthNameFromNumber(plant.TIMES.SOWING[0])}} - {{monthNameFromNumber(plant.TIMES.SOWING[1])}}</td>\
+            <td>{{monthNameFromNumber(plant.TIMES.SOWING[0])}} - {{monthNameFromNumber(plant.TIMES.SOWING[1], true)}}</td>\
           </tr>\
           <tr ng-if="plant.TIMES.PLANTING">\
             <td>planting</td>\
-            <td>{{monthNameFromNumber(plant.TIMES.PLANTING[0])}} - {{monthNameFromNumber(plant.TIMES.PLANTING[1])}}</td>\
+            <td>{{monthNameFromNumber(plant.TIMES.PLANTING[0])}} - {{monthNameFromNumber(plant.TIMES.PLANTING[1], true)}}</td>\
           </tr>\
           <tr>\
             <td>harvest</td>\
-            <td>{{monthNameFromNumber(plant.TIMES.HARVESTING[0])}} - {{monthNameFromNumber(plant.TIMES.HARVESTING[1])}}</td>\
+            <td>{{monthNameFromNumber(plant.TIMES.HARVESTING[0])}} - {{monthNameFromNumber(plant.TIMES.HARVESTING[1], true)}}</td>\
           </tr>\
           <tr ng-if="plant.TIMES.SOWING[2]">\
             <td>2nd sow</td>\
-            <td>{{monthNameFromNumber(plant.TIMES.SOWING[2])}} - {{monthNameFromNumber(plant.TIMES.SOWING[3])}}</td>\
+            <td>{{monthNameFromNumber(plant.TIMES.SOWING[2])}} - {{monthNameFromNumber(plant.TIMES.SOWING[3], true)}}</td>\
           </tr>\
           <tr ng-if="plant.TIMES.HARVESTING[2]">\
             <td>2nd harvest</td>\
-            <td>{{monthNameFromNumber(plant.TIMES.HARVESTING[2])}} - {{monthNameFromNumber(plant.TIMES.HARVESTING[3])}}</td>\
+            <td>{{monthNameFromNumber(plant.TIMES.HARVESTING[2])}} - {{monthNameFromNumber(plant.TIMES.HARVESTING[3], true)}}</td>\
           </tr>\
         </table>'
     };
